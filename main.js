514fc6b537d489044c0e07a49d3aa747626d3179
hoangviet2 <hoangviet18306@gmail.com>
2023-10-28 23:37:15 +0700

    let'sgo

---
+++ main.js
@@ -0,0 +1,31 @@
+function updateTimer() {
+	// yyyy-MM-dd HH:mm:ss
+  past  = Date.parse("2023-09-25 00:00:00");
+  now     = new Date();
+  diff    = now - past;
+
+  years  = Math.floor( diff / (1000*60*60*24*30*12) );
+  months = Math.floor( diff / (1000*60*60*24*30) );
+  days   = Math.floor( diff / (1000*60*60*24) );
+  hours  = Math.floor( diff / (1000*60*60) );
+  mins   = Math.floor( diff / (1000*60) );
+  secs   = Math.floor( diff / 1000 );
+
+  y = years;
+  M = months - years  * 12;
+  d = days   - months * 30;
+  h = hours  - days   * 24;
+  m = mins   - hours  * 60;
+  s = secs   - mins   * 60;
+  document.getElementById("timer")
+    .innerHTML =
+      '<div><span>Hoàng (Zịt + Lê)</span></div><br>' +
+      '<div><span>since: 25.09.2023</span></div><br>'+
+      '<div>' + y + '<span>years</span></div>' +
+      '<div>' + M + '<span>months</span></div>' +
+      '<div>' + d + '<span>days</span></div>' +
+      '<div>' + h + '<span>hours</span></div>' +
+      '<div>' + m + '<span>minutes</span></div>' +
+      '<div>' + s + '<span>seconds</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
